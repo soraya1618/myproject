fd9c015bf9056ccc75fee35d61324179f1a85dd9
soraya1618 <sorayyakhawari@gmail.com>
2025-01-28 14:39:16 +0100

    html-synthese

--- src/array.js
+++ src/array.js
@@ -54,31 +54,31 @@
 // }
 
 //Transponierung(jabjaii): untereinander ---> nebeneinander
-function getSentenceArr1(arr) {
-    const GAP = " ";
-    const PUNCT = ".";
-    let str = "";
-   //a. Variante: Struktur GENAU erzeugen.
-    // for (let i = 0; i < arr.length; i++) {
-    //    if (i != arr.length-1) {
-    //     str += arr[i] + GAP;
-    //    } else {
-    //     str += arr[i] + PUNCT;
-    //    };
+// function getSentenceArr1(arr) {
+//     const GAP = " ";
+//     const PUNCT = ".";
+//     let str = "";
+//    //a. Variante: Struktur GENAU erzeugen.
+//     // for (let i = 0; i < arr.length; i++) {
+//     //    if (i != arr.length-1) {
+//     //     str += arr[i] + GAP;
+//     //    } else {
+//     //     str += arr[i] + PUNCT;
+//     //    };
         
-    // }
-    //b: struktur erzeugen + trimmen
-    // for (let i = 0; i < arr.length-1; i++) {
-    //     str += arr[i] + GAP;
+//     // }
+//     //b: struktur erzeugen + trimmen
+//     // for (let i = 0; i < arr.length-1; i++) {
+//     //     str += arr[i] + GAP;
         
-    // }
-    // return str.trim() + PUNCT;
+//     // }
+//     // return str.trim() + PUNCT;
     
-    //c:Variante: Ausnutzen von generischen Fuktionen
-    return arr.join(GAP) + PUNCT;
-}
-output(getSentenceArr1(["Ich","bin","die","coole","Maxine","Mützerich"]));
-output(getSentenceArr1([4,5,3,8,11]));
+//     //c:Variante: Ausnutzen von generischen Fuktionen
+//     return arr.join(GAP) + PUNCT;
+// }
+// output(getSentenceArr1(["Ich","bin","die","coole","Maxine","Mützerich"]));
+// output(getSentenceArr1([4,5,3,8,11]));
 
 
 //1a. zu "Fuß"- DRY (don't repeat yourself) !!!
@@ -98,6 +98,116 @@ hier: Aufsummierung(jambandi)
 //     output(str);
 // }
 
-// function output(outputData){
-//     console.log(outputData);
-// }
+//console.log(" - - - - - - --  - - - - - - - - - - - - - - - -");
+
+
+
+//dinamisierung von Arrays
+// let arr = [];
+// output(arr);
+
+// arr.push("Ich");
+// output(arr);
+// arr.push("bin");
+// output(arr);
+// arr.push("Sorayya");
+// output(arr);
+ 
+// //pop
+
+// output(arr.pop());
+// output(arr);
+// output(arr.pop());
+// output(arr);
+// output(arr.pop());
+// output(arr);
+
+
+
+
+
+
+
+
+
+// Html code erzeugen
+const COBJ = {open_o: "<", close_o:"</",close:">"};
+const TAGS = [ "html", "head", "head", "body",
+                "h1", "h1",
+                "p", "p", 
+                "ul","li", "li", "li", "li","ul", 
+                "ol", "li", "li", "ol",  
+                "p","p",
+                "body","html"
+            ];
+
+const NEW_LINE_STR = "\n";
+const NEW_TAB_STR = "\t";
+
+let stack = [];
+
+
+//call
+output(generateHtml());
+// output(generateHtml(TAGS[1]));
+// output(generateHtml(TAGS[1]));
+
+function generateHtml() {
+    let htmlStr = "";
+    let count = 0;
+    let op = "";
+
+    for (let i = 0; i < TAGS.length; i++) {
+        if (generateOpenElement(TAGS[i])) {
+            count = stack.length-1;
+            op = "open";
+        } else {
+            count = stack.length;
+            op = "close";
+        }
+
+        htmlStr += getTabs(count) + getElement(TAGS[i],op) + NEW_LINE_STR;
+        }
+        return htmlStr;
+}
+
+function getTabs(count) {
+    let tabs = "";
+    for (let i = 0; i < count; i++) {
+        tabs += "\t";
+    }
+    return tabs;
+}
+
+function generateOpenElement(tag){
+
+    let cond = (tag != stack[stack.length-1]);
+    
+    if (cond) {
+        stack.push(tag);
+        output(stack);
+        return true;
+        
+    } else {
+        stack.pop(tag);
+        output(stack);
+        return false;
+    }
+}
+
+
+//zusammenbau der Elements
+function getElement(tag, op){
+    switch (op) {
+        case "open":
+            return COBJ.open_o + tag + COBJ.close;
+        case "close":
+            return COBJ.close_o + tag + COBJ.close;
+        default:
+            return "ERROR";
+    }
+}
+function output(outputData) {
+    console.log(outputData);
+}
+
